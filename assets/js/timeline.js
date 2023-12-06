1a896b27fb6feec0643fdade994c24c2d32d4d33
Akeko <artapp.theo@gmail.com>
2023-12-06 13:38:15 +0100

    add somethings

--- assets/js/timeline.js
+++ assets/js/timeline.js
@@ -5,7 +5,12 @@ class Timeline extends HTMLElement {
   }
 
   connectedCallback() {
-    //implementation
+    this.innerHTML = `
+    <section class="timeline">
+      <h2 class="timeline__title">Comprendre le principe de timeline dans les animations scroll-driven</h2>
+      <p class="timeline__paragraph">Il existe deux types de timeline. La première est la timeline</p>
+    </section>
+    `;
   }
 
   disconnectedCallback() {
