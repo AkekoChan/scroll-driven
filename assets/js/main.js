74adf0aac5c2786862550cc66f7490c58f522935
Akeko <artapp.theo@gmail.com>
2023-12-06 10:44:48 +0100

    Modal no support feature

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,38 @@
 import "./hero.js";
 import "./timeline.js";
+
+("use strict");
+(function () {
+  const App = {
+    DOM: {
+      revealElements: $$(".bottom-fade"),
+      closeNoSupport: $(".no-support__close"),
+    },
+
+    init: () => {
+      App.event();
+    },
+
+    event: () => {
+      App.DOM.closeNoSupport.addEventListener("click", App.closeModal);
+
+      App.DOM.closeNoSupport.addEventListener("keydown", (e) => {
+        if (e.key === " " || e.key === "Enter") {
+          e.preventDefault();
+          App.closeModal();
+        }
+      });
+      // console.log(App.DOM.closeNoSupport);
+      // App.DOM.revealElements.forEach((el, index) => {
+      //   el.style.animationDelay = `${index * 0.5}s`;
+      // });
+    },
+
+    closeModal: () => {
+      $(".no-support__blur").style.display = "none";
+      $(".no-support").style.display = "none";
+    },
+  };
+
+  window.addEventListener("DOMContentLoaded", App.init);
+})();
