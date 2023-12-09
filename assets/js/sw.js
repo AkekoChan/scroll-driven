29b65c8a1a26e5fe4d68af5a25104f7af743d679
Akeko <artapp.theo@gmail.com>
2023-12-09 18:56:23 +0100

    add clear cache

--- assets/js/sw.js
+++ assets/js/sw.js
@@ -31,3 +31,17 @@ self.addEventListener("fetch", (event) => {
     })
   );
 });
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== "static-cache") {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
